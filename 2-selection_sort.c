b5459f9b5dbb4ed9c2f501cd986da5ec8b9764d1
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-04-25 21:58:03 +0000

    Tried fixing some bugs

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,11 +12,17 @@
 
 void selection_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
-	{
-		size_t min_index = i;
+	size_t i, j, min_index;
+	int temp;
 
-		for (size_t j = i + 1; j < size; j++)
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 			{
@@ -26,7 +32,7 @@ void selection_sort(int *array, size_t size)
 
 		if (min_index != i)
 		{
-			int temp = array[i];
+			temp = array[i];
 
 			array[i] = array[min_index];
 			array[min_index] = temp;
