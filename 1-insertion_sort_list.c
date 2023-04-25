d56c1590a424a31adba0d2b7951a972e121e3079
Papa-Obeng <papaobengn@gmail.com>
2023-04-25 22:01:31 +0000

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,63 +3,44 @@
 #include "sort.h"
 
 /**
- * Definition for doubly-linked list.
- * struct listint_s {
- *     int n;
- *     struct listint_s *prev;
- *     struct listint_s *next;
- * };
- * typedef struct listint_s listint_t;
- */
-
-/**
- * insertion_sort_list - function that sorts a doubly linked list of integers.
- * @list: Pointer to the head of the doubly linked list.
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
  */
 void insertion_sort_list(listint_t **list)
 {
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	listint *node;
+
+	if (list == NULL || (*list)->next == NULL)
 		return;
-
-	listint_t *current = (*list)->next;
-
-	while (current != NULL)
+	node = (*list)->next;
+	while (node)
 	{
-
-		listint_t *temp = current;
-
-		while (temp->prev != NULL && temp->n < temp->prev->n)
+		while ((node->prev) && (node->prev->n > node->n))
 		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-				temp->next = temp->prev;
-				temp->prev = temp->next->prev;
-				temp->next->prev = temp;
-			if (temp->prev == NULL)
-				*list = temp;
-			else
-				temp->prev->next = temp;
-			printf("Current list: ");
+			node = swap_node(node, list);
 			print_list(*list);
 		}
-		current = current->next;
+		node = node->next;
 	}
 }
-
 /**
- * print_list - Prints a doubly linked list of integers.
- * @list: Pointer to the head of the doubly linked list.
+ * swap_node - swap a node for his previous one
+ * @node: node
+ * @list node list
+ * Return: return a pointer to a node
  */
-void print_list(const listint_t *list)
+listint_t *swap_node(listint_t *node, listint_t **list)
 {
+	listint_t *back = node->prev, *current = node;
 
-	while (list != NULL)
-	{
-		printf("%d", list->n);
-		if (list->next != NULL)
-			printf(", ");
-		list = list->next;
-	}
-	printf("\n");
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
